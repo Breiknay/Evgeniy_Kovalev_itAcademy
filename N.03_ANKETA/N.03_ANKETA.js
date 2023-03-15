4ac388bf562b44c4d75e68c416b6a0dedf5949ed
evgeniy <evgeniy.kovalev@bbv.by>
2023-03-15 09:30:55 +0300

    ANKETA

--- N.03_ANKETA/N.03_ANKETA.js
+++ N.03_ANKETA/N.03_ANKETA.js
@@ -1,27 +1,51 @@
 function getUserCardAlert() {
-    let name = prompt('Укажите пожалуйста свое имя');
-    name = checkString(name) === true ? name : "";
 
-    let lastName = prompt('Укажите пожалуйста свою фамилию');
-    lastName = checkString(lastName) === true ? lastName : "";
-    let surName = prompt('Укажите пожалуйста свое отчество');
-    surName = checkString(surName) === true ? surName : "";
-    switch ((name && lastName && surName) !== "" && (name && lastName && surName) !== null) {
-        case true:
+    for (; ;) {
+        name = prompt('Укажите пожалуйста свое имя');
+
+        if ((name) !== "" && (name !== null) && isNaN(parseFloat(name))) {
             break;
-        case false:
-            let isTryAgain = confirm("Вы ввели некорректные данные, попробовать еще раз?");
-            if (isTryAgain) return getUserCardAlert()
-            else return
+        }
+        let isTryAgain = confirm("Вы ввели некорректные данные, попробовать еще раз?");
+        if (!isTryAgain) {
+            return
+        }
+
     }
-    let userAge = prompt('Укажите пожалуйста возраст в годах')
-    switch (isFinite(Number(userAge)) && userAge !== null) {
-        case true:
+    let lastName;
+    for (let i = 0; ; i++) {
+        lastName = prompt('Укажите пожалуйста свою фамилию');
+        if ((lastName) !== "" && (lastName !== null) && isNaN(parseFloat(lastName))) {
             break;
-        case false:
-            let isTryAgain = confirm("Вы ввели некорректные данные, попробовать еще раз?");
-            if (isTryAgain) return getUserCardAlert()
-            else return
+        }
+        let isTryAgain = confirm("Вы ввели некорректные данные, попробовать еще раз?");
+        if (!isTryAgain) {
+            return
+        }
+    }
+    let surName;
+    for (; ;) {
+        surName = prompt('Укажите пожалуйста свое отчество');
+
+        if ((surName) !== "" && (surName !== null) && isNaN(parseFloat(surName))) {
+            break;
+        }
+        let isTryAgain = confirm("Вы ввели некорректные данные, попробовать еще раз?");
+        if (!isTryAgain) {
+            return
+        }
+    }
+    let userAge
+    for (; ;) {
+        userAge = prompt('Укажите пожалуйста возраст в годах');
+
+        if (isFinite(Number(userAge)) && userAge !== null) {
+            break;
+        }
+        let isTryAgain = confirm("Вы ввели некорректные данные, попробовать еще раз?");
+        if (!isTryAgain) {
+            return
+        }
     }
     let isCheckGender = confirm("Ваш пол - мужской?");
     let isGender = "Женский";
@@ -42,12 +66,6 @@ function getUserCardAlert() {
 Возраст, дней: ${isUserAgeInDay}
 Пол: ${isGender}
 Пенсионный возраст: ${isPension}`)
-
-}
-
-function checkString(str) {
-    return isNaN(parseFloat(str));
 }
 
 getUserCardAlert()
-
