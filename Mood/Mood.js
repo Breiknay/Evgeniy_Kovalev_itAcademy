4b138fa7f0d8ac30c47cf7e1b8f58123a8d30095
evgeniy <evgeniy.kovalev@bbv.by>
2023-03-27 11:07:21 +0300

    Mood

--- Mood/Mood.js
+++ Mood/Mood.js
@@ -4,19 +4,24 @@ function randomDiap(n, m) {
 
 function mood(colorsCount) {
     const colors = ['', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый'];
+    const usedColors = {};
 
     console.log('цветов: ' + colorsCount);
-    const usedColors = {};
     for (let i = 1; i <= colorsCount; i++) {
-        let n = randomDiap(1, 7);
-        let colorName = colors[n];
-        while (colorName in usedColors) {
-            n = randomDiap(1, 7);
-            colorName = colors[n];
-        }
+        const colorName = getRandomColor(colors, usedColors);
         usedColors[colorName] = true;
         console.log(colorName);
     }
+}
+
+function getRandomColor(colors, usedColors) {
+    let n = randomDiap(1, 7);
+    let colorName = colors[n];
+    while (colorName in usedColors) {
+        n = randomDiap(1, 7);
+        colorName = colors[n];
+    }
+    return colorName;
 }
 
 mood(3);
