ac2a26220782a71fb763b34d17d1016967f70380
evgeniy <evgeniy.kovalev@bbv.by>
2023-03-29 12:36:13 +0300

    Mood

--- Mood/Mood.js
+++ Mood/Mood.js
@@ -1,20 +1,44 @@
-function randomDiap(n, m) {
-    return Math.floor(Math.random() * (m - n + 1)) + n;
-}
-
-function mood(colorsCount) {
-    const colors = ['', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый'];
-    const usedColors = {};
-
-    console.log(`цветов: ${colorsCount}`);
-    for (let i = 1; i <= colorsCount; i++) {
-        let colorName;
-        do {
-            colorName = colors[randomDiap(1, 7)];
-        } while (colorName in usedColors);
-        usedColors[colorName] = true;
-        console.log(colorName);
+function buildWrapper(tagName) {
+    return function (text, attrs) {
+        var html = "<" + tagName;
+        for (var attr in attrs) {
+            html += " " + attr + "='" + attrs[attr].replace(/['"&<>]/g, function (match) {
+                switch (match) {
+                    case "'":
+                        return "&apos;";
+                    case "\"":
+                        return "&quot;";
+                    case "&":
+                        return "&amp;";
+                    case "<":
+                        return "&lt;";
+                    case ">":
+                        return "&gt;";
+                }
+            }) + "'";
+        }
+        html += ">" + text.replace(/['"&<>]/g, function (match) {
+            switch (match) {
+                case "'":
+                    return "&apos;";
+                case "\"":
+                    return "&quot;";
+                case "&":
+                    return "&amp;";
+                case "<":
+                    return "&lt;";
+                case ">":
+                    return "&gt;";
+            }
+        }) + "</" + tagName + ">";
+        return html;
     }
 }
 
-mood(3);
+var wrapP = buildWrapper("P");
+console.log(wrapP("Однажды в студёную зимнюю пору"));
+console.log(wrapP("Однажды в студёную зимнюю пору", {lang: "ru"}));
+console.log(wrapP("Однажды в <студёную> зимнюю пору"));
+
+var wrapH1 = buildWrapper("H1");
+console.log(wrapH1("СТИХИ", {align: "center", title: "M&M's"}));
