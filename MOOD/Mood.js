e3ccf89813a0c331fd0de5cc728fe85e307aceb6
Breiknay <99879147+Breiknay@users.noreply.github.com>
2023-03-29 12:45:51 +0300

    Update Mood.js

--- MOOD/Mood.js
+++ MOOD/Mood.js
@@ -4,22 +4,17 @@ function randomDiap(n, m) {
 
 function mood(colorsCount) {
     const colors = ['', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый'];
+    const usedColors = {};
 
-    console.log('цветов: ' + colorsCount);
+    console.log(`цветов: ${colorsCount}`);
     for (let i = 1; i <= colorsCount; i++) {
-        const n = randomDiap(1, 7);
-        const colorName = colors[n];
+        let colorName;
+        do {
+            colorName = colors[randomDiap(1, 7)];
+        } while (colorName in usedColors);
+        usedColors[colorName] = true;
         console.log(colorName);
     }
 }
 
 mood(3);
-
-// let used={}; // ключ хэша - число, которое уже встречалось
-// for ( let i=0; i<values.length; i++ ) {
-//     const value=values[i]; // очередное значение
-//     if ( value in used ) // встречалось ли оно?
-//         continue; // если да - всё, берём следующее
-//     used[value]=true; // если нет - запоминаем, что это значение уже встречалось
-//     console.log(value); // выводим его в консоль
-// }
