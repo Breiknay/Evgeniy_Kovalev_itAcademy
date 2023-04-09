0d8cf58d5e3714a0541770c0e617f57baec70b35
Breiknay <kovalovevgeny00@gmail.com>
2023-04-09 08:50:50 +0300

    HashStorageFunc

--- DRINKS_HASH_FUNC/DRINKS_HASH_FUNC.js
+++ DRINKS_HASH_FUNC/DRINKS_HASH_FUNC.js
@@ -2,8 +2,7 @@ function HashStorageFunc() {
     let myObject = {}
 
     this.addValue = function (key, value) {
-        myObject[`${key}`] = value
-        console.log(myObject)
+        myObject[key] = value
         return this;
     }
     this.getValue = function (key) {
@@ -23,11 +22,43 @@ function HashStorageFunc() {
     }
 }
 
-const hashStorageFunc = new HashStorageFunc();
-let forTest = {
-    name: "forTest"
+
+const drinkStorage = new HashStorageFunc();
+
+function makeDrink() {
+
+    let nameDrink = prompt("Введите название напитка")
+
+    let isAlcoholic = confirm("Он Алкогольный?")
+    let info = prompt("Введите Рецепт его приготовления")
+    let objectDrink = {
+        isAlcoholic: isAlcoholic,
+        info: info
+    }
+
+    drinkStorage.addValue(nameDrink, objectDrink)
+    document.getElementById("getDrink").style.visibility = "visible";
+    document.getElementById("deleteDrink").style.visibility = "visible";
 }
-hashStorageFunc.addValue(Object.keys(forTest)[0], forTest.name)
-hashStorageFunc.getValue(Object.keys(forTest)[0])
-hashStorageFunc.getKeys()
-console.log(hashStorageFunc.deleteValue(Object.keys(forTest)[0]))
+
+
+function getDrink() {
+    let nameDrink = prompt("Введите название напитка")
+    let info = drinkStorage.getValue(nameDrink)
+    if (!info) alert("Такого напитка не найдено")
+    alert(`напиток ${nameDrink}
+    алкогольный: ${info['isAlcoholic'] ? "Да" : "Нет"}
+    рецепт приготовления: ${info['info']}`)
+}
+
+function deleteDrink() {
+    let nameDrink = prompt("Введите название напитка")
+    let isDelete = drinkStorage.deleteValue(nameDrink)
+    alert(`${isDelete ? "Удалено успешно" : "Такого напитка не найдено"}`)
+}
+
+function getAllDrink() {
+    let info = drinkStorage.getKeys()
+    alert(info.length > 0 ? info : "Напитков нет")
+}
+
