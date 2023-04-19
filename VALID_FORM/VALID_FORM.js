0c3f63e85f751641ca521c6ef99f94b58d18acc6
Breiknay <kovalovevgeny00@gmail.com>
2023-04-19 21:50:03 +0300

    VALID_FORM

--- VALID_FORM/VALID_FORM.js
+++ VALID_FORM/VALID_FORM.js
@@ -1,99 +1,175 @@
+"use strict"
+
 function printError(elemId, hintMsg) {
     document.getElementById(elemId).innerHTML = hintMsg;
     document.getElementById(elemId).style.cssText = 'color: red;display: inline-block; text-align: right;';
 }
 
-// Определяем функции для проверки формы
-function validateForm() {
-    // Получение значений элементов формы
-    var name = document.contactForm.name.value;
-    var email = document.contactForm.email.value;
-    var mobile = document.contactForm.mobile.value;
-    var country = document.contactForm.country.value;
-    var gender = document.contactForm.gender.value;
-    var hobbies = [];
-    var checkboxes = document.getElementsByName("hobbies[]");
-    for(var i=0; i < checkboxes.length; i++) {
-        if(checkboxes[i].checked) {
-            // Заполняем массив хобби выбранными значениями
-            hobbies.push(checkboxes[i].value);
-        }
-    }
 
-    // Определяем переменные ошибок со значением по умолчанию
-    var nameErr = emailErr = mobileErr = countryErr = genderErr = true;
+let error = {
+    workersErr: true,
+    sitenameErr: true,
+    siteurlErr: true,
+    dateErr: true,
+    visitorsErr: true,
+    emailErr: true,
+    descriptionErr: true
+}
 
-    // Проверяем имя
-    if(name == "") {
-        printError("nameErr", "Пожалуйста, введите ваше имя");
+
+function toggleDisabled(_checked) {
+    document.form.description.disabled = _checked ? false : true;
+}
+
+
+form.addEventListener('submit', function (event) {
+    let workers = document.form.workers.value;
+    let sitename = document.form.sitename.value;
+    let siteUrlForYou = document.form.siteUrlForYou.value;
+    let dateNowTime = document.form.dateNowTime.value;
+    let visitors = document.form.visitors.value;
+    let email = document.form.email.value;
+    let description = document.form.description.value;
+
+
+    let workersFocus = document.form.workers
+    let regexForWorkers = /^[\p{L}\s]+$/u;
+    if (!workers) {
+        workersFocus.focus();
+        event.preventDefault()
+        printError("workersErr", "Пожалуйста, введите ваше имя");
     } else {
-        var regex = /^[a-zA-Z\s]+$/;
-        if(regex.test(name) === false) {
-            printError("nameErr", "Пожалуйста, введите правильное имя");
+
+        if (workers.length < 3) {
+            workersFocus.focus();
+            printError("workersErr", "Пожалуйста, введите полное имя");
+        } else if (regexForWorkers.test(workers) === false) {
+            workersFocus.focus();
+            printError("workersErr", "Пожалуйста, введите правильное имя");
         } else {
-            printError("nameErr", "");
-            nameErr = false;
+            printError("workersErr", "");
+            error.workersErr = false;
+        }
+    }
+    let sitenameFocus = document.form.sitename
+    if (!sitename) {
+        sitenameFocus.focus();
+        event.preventDefault()
+        printError("sitenameErr", "Пожалуйста, введите название сайта");
+    } else {
+        printError("sitenameErr", "");
+        error.sitenameErr = false;
+
+    }
+
+
+    let siteurlFocus = document.form.siteUrlForYou
+
+    if (!siteUrlForYou) {
+        siteurlFocus.focus();
+        event.preventDefault()
+        printError("siteurlErr", "Пожалуйста, введите название сайта");
+    } else {
+        if (siteUrlForYou.indexOf('https://') === -1) {
+            siteurlFocus.focus();
+            event.preventDefault()
+            printError("siteurlErr", "Пожалуйста, укажите действильный сайт");
+        } else {
+            printError("siteurlErr", "");
+            error.siteurlErr = false;
+
         }
     }
 
-    // Проверяем адрес электронной почты
-    if(email == "") {
+    let dateFocus = document.form.dateNowTime
+    if (!dateNowTime) {
+        dateFocus.focus();
+        event.preventDefault()
+        printError("dateErr", "Пожалуйста, укажите дату");
+    } else {
+
+        let targetDate = new Date(dateNowTime)
+
+        let currentDate = new Date();
+
+        if (targetDate.getTime() > currentDate.getTime()) {
+            dateFocus.focus();
+            event.preventDefault()
+            printError("dateErr", "Пожалуйста, нормальную дату");
+
+        } else {
+            printError("dateErr", "");
+            error.dateErr = false;
+        }
+
+    }
+
+
+    let visitorsFocus = document.form.visitors
+
+    if (!visitors) {
+        visitorsFocus.focus();
+        event.preventDefault()
+        printError("visitorsErr", "Пожалуйста, укажите количество посетителей");
+    } else {
+
+        if (visitors > '40') {
+            printError("visitorsErr", "Ого");
+            error.visitorsErr = false;
+        } else {
+            printError("visitorsErr", "");
+            error.visitorsErr = false;
+        }
+    }
+
+    let emailFocus = document.form.email
+    if (!email) {
+        emailFocus.focus();
         printError("emailErr", "Пожалуйста, введите адрес вашей электронной почты");
     } else {
-        // Регулярное выражение для базовой проверки электронной почты
-        var regex = /^\S+@\S+\.\S+$/;
-        if(regex.test(email) === false) {
+
+        let regex = /^\S+@\S+\.\S+$/;
+        if (regex.test(email) === false) {
+            visitorsFocus.focus();
             printError("emailErr", "Пожалуйста, введите действительный адрес электронной почты");
-        } else{
+        } else {
             printError("emailErr", "");
-            emailErr = false;
+            error.emailErr = false;
+        }
+    }
+    if (!document.form.description.disabled) {
+        let descriptionFocus = document.form.description
+
+        if (!description) {
+            descriptionFocus.focus();
+            event.preventDefault()
+            printError("descriptionErr", "Пожалуйста, заполните информацию о сайте");
+
+
+        } else {
+
+            printError("descriptionErr", "");
+            error.descriptionErr = false;
+        }
+    } else {
+
+        printError("descriptionErr", "");
+        error.descriptionErr = false;
+    }
+
+    let sent = true
+    for (var key in error) {
+        if (error.hasOwnProperty(key)) {
+            if (error[key] === true) {
+                sent = false
+            }
+
         }
     }
 
-    // Проверяем номер мобильного телефона
-    if(mobile == "") {
-        printError("mobileErr", "Пожалуйста, введите номер вашего мобильного телефона");
-    } else {
-        var regex = /^[1-9]\d{9}$/;
-        if(regex.test(mobile) === false) {
-            printError("mobileErr", "Пожалуйста, введите действительный 10-значный номер мобильного телефона");
-        } else{
-            printError("mobileErr", "");
-            mobileErr = false;
-        }
+    if (!sent) {
+        event.preventDefault()
     }
 
-    // Проверяем страну
-    if(country == "Select") {
-        printError("countryErr", "Пожалуйста, выберите вашу страну");
-    } else {
-        printError("countryErr", "");
-        countryErr = false;
-    }
+})
 
-    // Проверяем пол
-    if(gender == "") {
-        printError("genderErr", "Пожалуйста, выберите ваш пол");
-    } else {
-        printError("genderErr", "");
-        genderErr = false;
-    }
-
-    // Запрещаем отправку формы в случае ошибок
-    if((nameErr || emailErr || mobileErr || countryErr || genderErr) == true) {
-        return false;
-    } else {
-        // Создаем строки из входных данных для предварительного просмотра
-        var dataPreview = "Вы ввели следующие данные: \n" +
-            "Имя: " + name + "\n" +
-            "Email: " + email + "\n" +
-            "Номер: " + mobile + "\n" +
-            "Страна: " + country + "\n" +
-            "Пол: " + gender + "\n";
-        if(hobbies.length) {
-            dataPreview += "Hobbies: " + hobbies.join(", ");
-        }
-        // Отображаем входные данные в диалоговом окне перед отправкой формы
-        alert(dataPreview);
-    }
-};
