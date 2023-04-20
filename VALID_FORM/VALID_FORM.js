61a92a895d4ae723b1ad9cc90657089c25add120
evgeniy <evgeniy.kovalev@bbv.by>
2023-04-20 11:04:54 +0300

    VALID_FORM

--- VALID_FORM/VALID_FORM.js
+++ VALID_FORM/VALID_FORM.js
@@ -5,6 +5,7 @@ function printError(elemId, hintMsg) {
     document.getElementById(elemId).style.cssText = 'color: red;display: inline-block; text-align: right;';
 }
 
+const regexForWorkers = /^[\p{L}\s]+$/u;
 
 let error = {
     workersErr: true,
@@ -13,13 +14,111 @@ let error = {
     dateErr: true,
     visitorsErr: true,
     emailErr: true,
-    descriptionErr: true
+    descriptionErr: true,
+    paymentErr: true,
+    votesErr: true,
+    typeSiteErr: true
 }
 
 
-function toggleDisabled(_checked) {
-    document.form.description.disabled = _checked ? false : true;
-}
+form.workers.addEventListener("blur", function () {
+    let workers = document.form.workers.value;
+    if (!workers) {
+        printError("workersErr", "Пожалуйста, введите ваше имя");
+    } else {
+        if (workers.length < 3) {
+            printError("workersErr", "Пожалуйста, введите полное имя");
+        } else if (regexForWorkers.test(workers) === false) {
+            printError("workersErr", "Пожалуйста, введите правильное имя");
+        } else {
+            printError("workersErr", "");
+        }
+    }
+})
+
+
+form.sitename.addEventListener("blur", function () {
+    let sitename = document.form.sitename.value;
+    if (!sitename) {
+        printError("sitenameErr", "Пожалуйста, введите название сайта");
+    } else {
+        printError("sitenameErr", "");
+    }
+})
+
+form.siteUrlForYou.addEventListener("blur", function () {
+    let siteUrlForYou = document.form.siteUrlForYou.value;
+    if (!siteUrlForYou) {
+        printError("siteurlErr", "Пожалуйста, введите название сайта");
+    } else {
+        if (siteUrlForYou.indexOf('https://') === -1) {
+            printError("siteurlErr", "Пожалуйста, укажите действильный сайт");
+        } else {
+            printError("siteurlErr", "");
+        }
+    }
+})
+
+
+form.dateNowTime.addEventListener("blur", function (event) {
+    let dateNowTime = document.form.dateNowTime.value;
+
+    if (!dateNowTime) {
+        event.preventDefault()
+        printError("dateErr", "Пожалуйста, укажите дату");
+    } else {
+
+        let targetDate = new Date(dateNowTime)
+        let currentDate = new Date();
+        if (targetDate.getTime() > currentDate.getTime()) {
+            printError("dateErr", "Пожалуйста, нормальную дату");
+        } else {
+            printError("dateErr", "");
+        }
+    }
+})
+
+
+form.visitors.addEventListener("blur", function () {
+    let visitors = document.form.visitors.value;
+    if (!visitors) {
+        printError("visitorsErr", "Пожалуйста, укажите количество посетителей");
+    } else {
+        if (visitors > '40') {
+            printError("visitorsErr", "Ого");
+        } else {
+            printError("visitorsErr", "");
+        }
+    }
+})
+form.email.addEventListener("blur", function (event) {
+    let email = document.form.email.value;
+    if (!email) {
+        printError("emailErr", "Пожалуйста, введите адрес вашей электронной почты");
+    } else {
+
+        let regex = /^\S+@\S+\.\S+$/;
+        if (regex.test(email) === false) {
+            printError("emailErr", "Пожалуйста, введите действительный адрес электронной почты");
+        } else {
+            printError("emailErr", "");
+        }
+    }
+})
+form.description.addEventListener("blur", function (event) {
+    let description = document.form.description.value;
+    if (!description) {
+        printError("descriptionErr", "Пожалуйста, заполните информацию о сайте");
+    } else {
+
+        if (description.length < 10) {
+            printError("descriptionErr", "Пожалуйста, нужно больше информации");
+        } else {
+            printError("descriptionErr", "");
+        }
+
+    }
+})
 
 
 form.addEventListener('submit', function (event) {
@@ -30,6 +129,9 @@ form.addEventListener('submit', function (event) {
     let visitors = document.form.visitors.value;
     let email = document.form.email.value;
     let description = document.form.description.value;
+    let payment = document.form.payment.value;
+    let votes = document.getElementsByName("votes");
+    let typeSite = document.form.typeSite.value;
 
 
     let workersFocus = document.form.workers
@@ -137,24 +239,46 @@ form.addEventListener('submit', function (event) {
             error.emailErr = false;
         }
     }
-    if (!document.form.description.disabled) {
-        let descriptionFocus = document.form.description
-
-        if (!description) {
-            descriptionFocus.focus();
-            event.preventDefault()
-            printError("descriptionErr", "Пожалуйста, заполните информацию о сайте");
 
 
-        } else {
+    if (!payment) {
+        printError("paymentErr", "Пожалуйста, укажите, где будет размещение");
+    } else {
+        printError("paymentErr", "");
+        error.paymentErr = false;
+    }
 
-            printError("descriptionErr", "");
-            error.descriptionErr = false;
-        }
+    let descriptionFocus = document.form.description
+    if (!description) {
+        descriptionFocus.focus();
+        printError("descriptionErr", "Пожалуйста, заполните информацию о сайте");
     } else {
 
         printError("descriptionErr", "");
         error.descriptionErr = false;
+    }
+
+
+    if (payment === "3") {
+        if ((votes[0].checked) === false) {
+            printError("votesErr", "Если выбрано размещение VIP, то возможность оставлять отзывы обязательно");
+        } else {
+            printError("votesErr", "");
+            error.votesErr = false;
+        }
+
+    } else {
+        printError("paymentErr", "");
+        error.votesErr = false;
+    }
+
+
+    if (typeSite === "Select") {
+        printError("typeSiteErr", "Пожалуйста, выберите типа сайта");
+    } else {
+        printError("typeSiteErr", "");
+        error.typeSiteErr = false;
+
     }
 
     let sent = true
