744cf3cea9f7d68bcea890473bc52d23df7fc192
Breiknay <kovalovevgeny00@gmail.com>
2023-05-09 12:53:07 +0300

    CLOCK_DOM

--- B3+/B3+.js
+++ B3+/B3+.js
@@ -1,106 +1,74 @@
 function calculate(expression) {
-    let result = 0;
-    let operator = '+';
-    let numBuffer = '';
-    const stack = [];
-    let isUnaryMinus = true;
+    let tokens = expression.match(/(\d+(\.\d+)?)|([()+\-*/])/g);
 
-    for (let i = 0; i < expression.length; i++) {
-        const char = expression[i];
+    function peek() {
+        if (tokens) {
+            return tokens[0];
+        }
 
-        if (/[\d.]/.test(char)) {
-            numBuffer += char;
-            isUnaryMinus = false;
-        } else if (char === '-') {
-            if (i === 0 || /[\+\-\*\/\(]/.test(expression[i - 1])) {
-                isUnaryMinus = true;
-            } else if (isUnaryMinus) {
-                operator = '-';
-                isUnaryMinus = false;
-            } else {
-                const num = parseFloat(numBuffer);
-                if (!isNaN(num)) {
-                    switch (operator) {
-                        case '+':
-                            result += num;
-                            break;
-                        case '-':
-                            result -= num;
-                            break;
-                        case '*':
-                            result *= num;
-                            break;
-                        case '/':
-                            result /= num;
-                            break;
-                    }
-                    numBuffer = '';
-                }
-                operator = '-';
+    }
+
+    function get() {
+        if (tokens) {
+            return tokens.shift();
+        }
+    }
+
+    function parsePrimaryExpression() {
+        let token = get();
+
+        if (/\d+(\.\d+)?/.test(token)) {
+            return parseFloat(token);
+        } else if (token === '(') {
+            let expr = parseExpression();
+            if (get() !== ')') {
+                printError("Error", "Пожалуйста, введите правильное значение");
+                return
             }
+            return expr;
+        } else if (token === '-') {
+            return -parsePrimaryExpression();
         } else {
-            const num = parseFloat(numBuffer);
-            if (!isNaN(num)) {
-                switch (operator) {
-                    case '+':
-                        result += num;
-                        break;
-                    case '-':
-                        result -= num;
-                        break;
-                    case '*':
-                        result *= num;
-                        break;
-                    case '/':
-                        result /= num;
-                        break;
-                }
-                numBuffer = '';
-            }
-            isUnaryMinus = true;
-            if (char === '(') {
-                stack.push(result);
-                stack.push(operator);
-                result = 0;
-                operator = '+';
-            } else if (char === ')') {
-                const prevOperator = stack.pop();
-                const prevResult = stack.pop();
-                switch (prevOperator) {
-                    case '+':
-                        result = prevResult + result;
-                        break;
-                    case '-':
-                        result = prevResult - result;
-                        break;
-                    case '*':
-                        result = prevResult * result;
-                        break;
-                    case '/':
-                        result = prevResult / result;
-                        break;
-                }
-            } else if (/[\+\-\*\/]/.test(char)) {
-                operator = char;
-            }
+            printError("Error", "Пожалуйста, введите правильное значение");
+            return
         }
     }
-    const num = parseFloat(numBuffer);
-    if (!isNaN(num)) {
-        switch (operator) {
-            case '+':
-                result += num;
-                break;
-            case '-':
-                result -= num;
-                break;
-            case '*':
-                result *= num;
-                break;
-            case '/':
-                result /= num;
-                break;
+
+    function parseMultiplicativeExpression() {
+        let expr = parsePrimaryExpression();
+        while (/[*/]/.test(peek())) {
+            let op = get();
+            let rhs = parsePrimaryExpression();
+            if (op === '*') {
+                expr *= rhs;
+            } else {
+                expr /= rhs;
+            }
         }
+
+        return expr;
     }
-    return result;
+
+    function parseExpression() {
+        printError("Error", "");
+        let expr = parseMultiplicativeExpression();
+        while (/[-+]/.test(peek())) {
+            let op = get();
+            let rhs = parseMultiplicativeExpression();
+            if (op === '+') {
+                expr += rhs;
+            } else {
+                expr -= rhs;
+            }
+        }
+
+        return expr;
+    }
+
+    return parseExpression();
+}
+
+function printError(elemId, hintMsg) {
+    document.getElementById(elemId).innerHTML = hintMsg;
+    document.getElementById(elemId).style.cssText = 'color: red;display: inline-block; text-align: right;';
 }
