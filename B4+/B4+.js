5267fccfa5ab811dbd18ae5a9472e0f45d7cff3c
Breiknay <kovalovevgeny00@gmail.com>
2023-05-26 18:33:22 +0300

    B4+

---
+++ B4+/B4+.js
@@ -0,0 +1,46 @@
+"use strict";
+
+function findShortestChain(str1, str2) {
+    let words = ['ТАРА', 'ЛИПА', 'ТУРА', 'ЛУЖА', 'ПАРК', 'ЛОЖЬ', 'ЛУПА', 'ПЛОТ', 'МУРА', 'ПАУК', 'ПАУТ', 'ПЛУТ', 'ЛОЖА', 'СЛОТ', 'ПАРА'];
+    words.push(str2);
+    let used = {};
+    return findСouple(str1, str2, words).reduce(delimiter)
+
+    function delimiter(first, second) {
+        return first + '-' + second;
+    }
+
+    function findСouple(str1, str2, words) {
+        let wordsCount = {};
+        used[str1] = true;
+        let word
+        for (let i = 0; i < words.length; i++) {
+            word = words[i];
+            let n = 0;
+            for (let k = 0; k < str1.length; k++) if (str1.charAt(k) !== word.charAt(k)) n++;
+            wordsCount[word] = n;
+        }
+        let wordsCountArr = [];
+        for (word in wordsCount) wordsCountArr.push({word: word, count: wordsCount[word]});
+
+        function compareFunc(a, b) {
+            return a.count - b.count;
+        }
+
+        wordsCountArr.sort(compareFunc);
+
+        for (let z = 0; z < words.length; z++) {
+            let wordCount = wordsCountArr[z];
+            words[z] = wordCount.word;
+        }
+        let result = words.shift();
+        while (str1 !== str2) {
+            used[result] = true;
+            return findСouple(result, str2, words, used);
+        }
+        return Object.keys(used);
+    }
+
+}
+
+console.log(findShortestChain('ЛИCА', 'ЛОСЬ'))
