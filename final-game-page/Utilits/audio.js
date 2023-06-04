e3cc09991e055dbd8398d5cb425e3c50c8b4372c
Breiknay <kovalovevgeny00@gmail.com>
2023-06-04 22:16:02 +0300

    Final_game

---
+++ final-game-page/Utilits/audio.js
@@ -0,0 +1,5 @@
+export const MAIN_AUDIO = new Audio('Audio/Main.mp3');
+export const PENCIL_AUDIO = new Audio('Audio/Pencil.mp3')
+export const ERROR_AUDIO = new Audio('Audio/Error.mp3')
+export const WIN_AUDIO = new Audio('Audio/Win.mp3')
+
